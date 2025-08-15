db08c9aa9cb9ddc01a807cd8708d84e088e549e6
Abdullah Hasan <AbdullahAbdelhady929@gmail.com>
2025-08-15 06:07:30 +0300

    q2

--- C44-G01-OOP01/Program.cs
+++ C44-G01-OOP01/Program.cs
@@ -17,5 +17,25 @@
             }
         }
         #endregion
+        #region Question 2 - Person Struct
+        struct Person
+        {
+            public string Name;
+            public int Age;
+        }
+
+        static void DisplayPersons()
+        {
+            Person[] people = new Person[3];
+            people[0] = new Person { Name = "Ahmed", Age = 25 };
+            people[1] = new Person { Name = "Mohamed", Age = 30 };
+            people[2] = new Person { Name = "Fatima", Age = 22 };
+
+            foreach (var person in people)
+            {
+                Console.WriteLine($"Name: {person.Name}, Age: {person.Age}");
+            }
+        }
+        #endregion
     }
 }
