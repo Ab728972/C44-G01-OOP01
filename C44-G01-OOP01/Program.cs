37282e3c6f534c98219aa2f882b44a3a7f68d0a3
Abdullah Hasan <AbdullahAbdelhady929@gmail.com>
2025-08-15 06:06:55 +0300

    q1

--- C44-G01-OOP01/Program.cs
+++ C44-G01-OOP01/Program.cs
@@ -4,7 +4,18 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            
         }
+        #region Question 1 - WeekDays Enum
+        enum WeekDays { Monday, Tuesday, Wednesday, Thursday, Friday, Saturday, Sunday }
+
+        static void PrintWeekDays()
+        {
+            foreach (WeekDays day in Enum.GetValues(typeof(WeekDays)))
+            {
+                Console.WriteLine(day);
+            }
+        }
+        #endregion
     }
 }
